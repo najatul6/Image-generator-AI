f8ef7edc385d2647446b3a82d91d29bb22981e41
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-16 20:17:09 +0600

    create footer

--- Image-generator-AI-Client/src/Layout/MainLayout.jsx
+++ Image-generator-AI-Client/src/Layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../Component/NavBar/NavBar";
 
 
 const MainLayout = () => {
     return (
         <div>
+            <NavBar/>
             <Outlet/>
         </div>
     );
