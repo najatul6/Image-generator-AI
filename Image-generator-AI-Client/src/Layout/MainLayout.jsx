a36049ebec53a91c175d070df784cea4f0e2117c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-16 20:24:00 +0600

    add minimum hight on outlet

--- Image-generator-AI-Client/src/Layout/MainLayout.jsx
+++ Image-generator-AI-Client/src/Layout/MainLayout.jsx
@@ -2,15 +2,16 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../Component/NavBar/NavBar";
 import Footer from "../Component/Footer/Footer";
 
-
 const MainLayout = () => {
-    return (
-        <div>
-            <NavBar/>
-            <Outlet/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <div className="min-h-screen">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
