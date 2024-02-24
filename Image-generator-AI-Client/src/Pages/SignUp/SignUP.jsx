48ed804054dcaf999b3871f5801b4bd4623f21da
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-25 03:55:10 +0600

    Create sign up and lsign in route

---
+++ Image-generator-AI-Client/src/Pages/SignUp/SignUP.jsx
@@ -0,0 +1,10 @@
+
+const SignUP = () => {
+    return (
+        <div>
+            Sign Up
+        </div>
+    );
+};
+
+export default SignUP;
--- Image-generator-AI-Client/src/Router/Router.jsx
+++ Image-generator-AI-Client/src/Router/Router.jsx
@@ -4,35 +4,40 @@ import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
 import Contact from "../Pages/Contact/Contact";
 import PrivacyPolicy from "../Pages/PrivacyPolicy/PrivacyPolicy";
-
+import LogIn from "../Pages/LogIn/LogIn";
+import SignUP from "../Pages/SignUp/SignUP";
 
 const routes = createBrowserRouter([
-    {
-        path:"/",
-        element:<MainLayout/>,
-        children:[
-            {
-                path:"/",
-                element:<Home/>,
-            },
-            {
-                path:"about",
-                element:<About/>,
-            },
-            {
-                path:"contact",
-                element:<Contact/>,
-            },
-            {
-                path:"privacyPolicy",
-                element:<PrivacyPolicy/>,
-            },
-        ],
-    },
-    {
-        path:'/logIn',
-        element:
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "privacyPolicy",
+        element: <PrivacyPolicy />,
+      },
+    ],
+  },
+  {
+    path: "/logIn",
+    element: <LogIn />,
+  },
+  {
+    path: "/signUp",
+    element: <SignUP />,
+  },
+]);
 
 export default routes;
