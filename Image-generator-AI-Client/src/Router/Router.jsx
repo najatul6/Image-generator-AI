1b6d0d35f3ba6b8734011ec5096a77553f149f9f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-25 03:51:31 +0600

    Create about, contact and privacy pages routes

--- Image-generator-AI-Client/src/Router/Router.jsx
+++ Image-generator-AI-Client/src/Router/Router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Contact from "../Pages/Contact/Contact";
+import PrivacyPolicy from "../Pages/PrivacyPolicy/PrivacyPolicy";
 
 
 const routes = createBrowserRouter([
@@ -11,8 +14,24 @@ const routes = createBrowserRouter([
             {
                 path:"/",
                 element:<Home/>,
-            }
-        ]
+            },
+            {
+                path:"about",
+                element:<About/>,
+            },
+            {
+                path:"contact",
+                element:<Contact/>,
+            },
+            {
+                path:"privacyPolicy",
+                element:<PrivacyPolicy/>,
+            },
+        ],
+    },
+    {
+        path:'/logIn',
+        element:
     }
 ])
 
