c875dc211a1d207c665266ee8ecf7aaac156f1b4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-25 05:25:36 +0600

    edit backgorund and footer

--- Image-generator-AI-Client/src/Component/Footer/Footer.jsx
+++ Image-generator-AI-Client/src/Component/Footer/Footer.jsx
@@ -1,11 +1,47 @@
-import logo from "../../assets/Hotel NiN.png"
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/NiN.png"
+
 const Footer = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink
+          to="/about"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-light-sky" : "text-white"
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/privacyPolicy"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-light-sky" : "text-white"
+          }
+        >
+          Privacy Policy
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-light-sky" : "text-white"
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </>
+  );
   return (
     <footer className="bg-white rounded-t-xl shadow dark:bg-gray-900">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-2">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="https://flowbite.com/"
+          <Link
+            to="https://najatul6-cbc29.web.app/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <img
@@ -13,42 +49,20 @@ const Footer = () => {
               className="h-8"
               alt="Brand Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              NiN.AI
-            </span>
-          </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
+          </Link>
+          <ul className="flex flex-wrap gap-3 items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            {links}
           </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
-          </a>
-          . All Rights Reserved.
-        </span>
       </div>
+        <hr className="my-1 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-white sm:text-center dark:text-white font-medium pb-4">
+         &copy; 2024{" "}
+          <a href="https://najatul6-cbc29.web.app/" className="hover:underline mr-2">
+          NiN<sup>AI</sup>.
+          </a>
+            All Rights Reserved.
+        </span>
     </footer>
   );
 };
