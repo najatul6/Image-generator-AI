81e50cd19562d94ca3c7f8dd4a9043c5bcef1aef
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-25 06:53:25 +0600

    fixed width for form

--- Image-generator-AI-Client/src/Pages/LogIn/LogIn.jsx
+++ Image-generator-AI-Client/src/Pages/LogIn/LogIn.jsx
@@ -1,11 +1,13 @@
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 const LogIn = () => {
   return (
     <div className="bg-dark-blue min-h-screen mx-auto flex justify-center items-center">
-      <div className="w-3/5 flex justify-center items-center bg-low-blue mx-auto py-10 px-5 rounded-xl shadow-lg shadow-slate-400">
+      <div className="md:w-3/5 flex justify-center items-center bg-low-blue mx-auto py-10 px-5 rounded-xl shadow-lg shadow-slate-400">
         <div className="w-full">
-          <h2 className="flex justify-center items-center text-2xl font-bold py-3 uppercase text-white">
+          <h2 className="flex flex-col gap-2 justify-center items-center text-2xl font-bold py-3 uppercase text-white">
             Log In
+            <p className="text-xs font-normal">Sign in to access your account</p>
           </h2>
           <form className="transition-all duration-300">
             <div className="mb-5">
@@ -48,10 +50,21 @@ const LogIn = () => {
             </button>
           </form>
           <hr className="my-1 border-white sm:mx-auto dark:border-white lg:my-8" />
-          <button>
-            <FcGoogle />
-            Continue with Google
-          </button>
+          <div className="flex flex-col justify-center items-center">
+            <button className="flex justify-center items-center gap-2 text-white font-bold py-3 px-10 mb-3 hover:bg-dark-orange border">
+              <span>
+                <FcGoogle />
+              </span>
+              Continue with Google
+            </button>
+            <p className="text-white">
+              Don&rsquo;t have an account yet?{" "}
+              <Link to="/signUp" className="text-dark-orange font-bold hover:text-light-sky hover:underline">
+                Sign up
+              </Link>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </div>
