2f2d7c487d1973ed3acd4e41344962a50122bf67
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-25 06:01:47 +0600

    Create dropdown menu

--- Image-generator-AI-Client/src/Component/NavBar/NavBar.jsx
+++ Image-generator-AI-Client/src/Component/NavBar/NavBar.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/NiN.png"
+import logo from "../../assets/NiN.png";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [open, isOpen] = useState(false);
   const links = (
     <>
       <li>
@@ -10,6 +12,7 @@ const NavBar = () => {
           className={({ isActive, isPending }) =>
             isPending ? "pending" : isActive ? "text-light-sky" : "text-white"
           }
+          aria-current="page"
         >
           Home
         </NavLink>
@@ -40,12 +43,7 @@ const NavBar = () => {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img
-            src={logo}
-            className="h-14 "
-            alt="NiN AI Logo"
-          />
-          
+          <img src={logo} className="h-14 " alt="NiN AI Logo" />
         </a>
         <button
           data-collapse-toggle="navbar-default"
@@ -53,8 +51,9 @@ const NavBar = () => {
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
+          onClick={()=>isOpen(!open)}
         >
-          <span className="sr-only">Open main menu</span>
+          <span className="sr-only text-white">Open main menu</span>
           <svg
             className="w-5 h-5"
             aria-hidden="true"
@@ -75,6 +74,9 @@ const NavBar = () => {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {links}
           </ul>
+        </div>
+        <div className="md:hidden absolute top-16 w-full right-0 left-0 transition-all duration-1000">
+          <ul style={{ display: open ? 'block' : 'none' }} className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">{links}</ul>
         </div>
       </div>
     </nav>
